6acc9a9e42396724e6bd271a57f96702382d9921
huynhthienthe <huynhthienthetv2020@gmail.com>
2024-09-13 22:05:17 +0700

    shape: box -rotating box - cricle

--- HocThreeJSSuongSuong/src/App.jsx
+++ HocThreeJSSuongSuong/src/App.jsx
@@ -9,7 +9,6 @@ function App() {
   return (
     <>
       <div>
-        d
       </div>
       <span className="pt-5"></span>
     </>
--- HocThreeJSSuongSuong/src/main.js
+++ HocThreeJSSuongSuong/src/main.js
@@ -9,24 +9,53 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight,false );
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// Get shape:
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cricleGeometry = new THREE.TetrahedronGeometry(1, 16, 1);
 
-const cube = new THREE.Mesh(geometry, material);
+// Get Material
+const box_material = new THREE.MeshBasicMaterial({ color: 0x00ff00});
+const boxrotation_material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const Cricle_material = new THREE.MeshBasicMaterial({ color: 0x000ff0});
 
+// Get Green cube and Red cube: 
+const cube = new THREE.Mesh(boxGeometry, box_material);
+const cubeRotation = new THREE.Mesh(boxGeometry, boxrotation_material);
+const cricle = new THREE.Mesh(cricleGeometry, Cricle_material);
 
-scene.add(cube);
+// setup camera and cube position:
+// box - rotation box - cricle
+// camera:
 camera.position.z = 5;
+camera.position.y = 3;
+// shape:
+cubeRotation.position.x = 1.5;
+cricle.position.x =  cube.position.x - 2 ;
 
-const axesHeplper = new THREE.AxesHelper(2);
-cube.add(axesHeplper)
+// add shapes to Mesh
+scene.add(cube, cubeRotation, cricle);
 
+// Get axe
+const axesHeplper1 = new THREE.AxesHelper(2);
+const axesHeplper2 = new THREE.AxesHelper(2);
+const axesHeplper3 = new THREE.AxesHelper(2);
+
+// add axe to cube:
+cube.add(axesHeplper1);
+cubeRotation.add(axesHeplper2);
+cricle.add(axesHeplper3);
+
+// animations:
 function animate() {
+	cubeRotation.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
 
+// loop:
 renderer.setAnimationLoop(animate);
-	const controls = new OrbitControls( camera, renderer.domElement );
-	controls.addEventListener( 'change', render ); // use if there is no animation loop
+
+// camera router:
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.addEventListener( 'change', render); // use if there is no animation loop
 
 
