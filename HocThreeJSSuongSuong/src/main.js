758063542201dcbd2b5b5cab19aa8d93c7cf3a5f
huynhthienthe <huynhthienthetv2020@gmail.com>
2024-09-30 21:53:56 +0700

    code tùm lum

--- HocThreeJSSuongSuong/src/main.js
+++ HocThreeJSSuongSuong/src/main.js
@@ -6,12 +6,42 @@ const scene = new THREE.Scene();
 
 // add objects to the scene
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const cubeMaterial = new THREE.MeshBasicMaterial({color: "red", wireframe: true});
+
+// const vertices = new Float32Array([
+//     2, 2, 0,
+//     0, 0, 2,
+//     0, 2, 0
+// ])
+
+//const bufferAttribute = new THREE.BufferAttribute(vertices, 3);
+
+// const geometry = new THREE.BufferGeometry();
+// geometry.setAttribute('position', bufferAttribute)
+
+
+
+// const cubeMaterial = new THREE.MeshBasicMaterial({
+//    color: "white",
+//     wireframe: false,
+//     opacity: 0.2,
+    
+// });
+
+const cubeMaterial = new THREE.MeshLambertMaterial();
+
+const torusKnotGeometry = new THREE.TorusKnotGeometry(0.5, 0.15, 100, 16)
 
 const cubeMesh = new THREE.Mesh(
-    cubeGeometry,
+     cubeGeometry,
+    //geometry,
     cubeMaterial
 )
+
+// độ bóng bề mặt
+const material = new THREE.MeshPhongMaterial()
+material.shininess = 200
+material.color = new THREE.Color('green')
+
 
 const cubeMesh2 = new THREE.Mesh(
     cubeGeometry,
@@ -20,37 +50,56 @@ const cubeMesh2 = new THREE.Mesh(
 cubeMesh2.position.x = 2;
 
 const cubeMesh3 = new THREE.Mesh(
-    cubeGeometry,
-    cubeMaterial
+    torusKnotGeometry,
+    material
 )
 cubeMesh3.position.x = -2;
 
-const group = new THREE.Group();
-group.add(cubeMesh);
-group.add(cubeMesh2);
-group.add(cubeMesh3);
+
+ const group = new THREE.Group();
+ group.add(cubeMesh);
+ group.add(cubeMesh2);
+ group.add(cubeMesh3);
+
+//const fog = new THREE.Fog(0xffffff, 1, 10)
+//scene.fog = fog
+
+//scene.background = new THREE.Color('white')
+
+scene.add(group)
+//scene.add(cubeMesh)
+
+// cubeMesh.rotation.reorder('XYZ')
+// cubeMesh.rotation.y = 2;
+// cubeMesh.rotation.y = THREE.MathUtils.degToRad(90)
 
 
-//scene.add(group)
-scene.add(cubeMesh)
-
-cubeMesh.rotation.reorder('XYZ')
-cubeMesh.rotation.y = 2;
-cubeMesh.rotation.y = THREE.MathUtils.degToRad(90)
-
-
-cubeMesh.position.y = 1
-cubeMesh.position.z = 1
+//cubeMesh.position.y = 1
+//cubeMesh.position.z = 1
 
 //cubeMesh.scale.y = 2
 
 //cubeMesh.scale.set(2, 2, 3)
 
 const secneAxesHepler = new THREE.AxesHelper(2);
-const cubeAxesHepler = new THREE.AxesHelper(2);
+//const cubeAxesHepler = new THREE.AxesHelper(2);
 
 scene.add(secneAxesHepler);
-cubeMesh.add(cubeAxesHepler)
+//cubeMesh.add(cubeAxesHepler)
+
+
+const light = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(light)
+
+
+const pointLight = new THREE.PointLight(0xffffff, 1)
+pointLight.position.set(5, 5, 5)
+scene.add(pointLight)
+
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 5); // độ sáng của ánh sáng
+directionalLight.position.set(5, 5, 5).normalize();
+scene.add(directionalLight);
 
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -94,11 +143,13 @@ const renderloop = () => {
     const currentTime = clock.getElapsedTime()
     const delta = currentTime - previousTime
     previousTime = currentTime
+    
+    cubeMaterial.needsUpdate = true;
 
-    cubeMesh.rotation.y += THREE.MathUtils.degToRad(1) * delta * 10
+//cubeMesh.rotation.y += THREE.MathUtils.degToRad(1) * delta * 10
 
-    Math.sin(currentTime)
-   cubeMesh.position.x = (Math.sin(currentTime)) * 3 + 2
+//    Math.sin(currentTime)
+  // cubeMesh.position.x = (Math.sin(currentTime)) * 3 + 2
     controls.update(); render.render(scene, camera)
     window.requestAnimationFrame(renderloop)
 }
